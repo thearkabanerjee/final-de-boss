10b87d5b4ac6f7f11014b88ad1a8199fcb2860cc
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-04 21:16:09 +0530

    final de boss

--- importing.py
+++ importing.py
@@ -2,13 +2,20 @@
 
 # print (math.log(10))
 
+# trying to simulate a coin toss
+# import random
+
+
+# a = random.random()
+# if a <= 0.5 :
+#     print ('Heads')
+# else:
+#     print ('Tails')
+# # print (random.random())
+
+
+# trying to simulate a dice
 
 import random
 
-
-a = random.random()
-if a <= 0.5 :
-    print ('Heads')
-else:
-    print ('Tails')
-# print (random.random())
+print (random.randrange(1,7))
