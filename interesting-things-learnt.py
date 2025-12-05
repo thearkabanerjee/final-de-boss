c960ae3f516fa7adce5d66024e54654149560b56
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-05 09:54:54 +0530

    final de boss

--- interesting-things-learnt.py
+++ interesting-things-learnt.py
@@ -1,3 +1,8 @@
 number = int (input ('enter the number: '))
 for i in range (1, 11):
     print ('{0} * {1} = {2}'. format(number, i, number* i))
+
+# another way of writing the same thing
+number = int (input ('enter the number: '))
+for i in range (1, 11):
+    print ('%d x %d = %d'%(number, i, number * i))
