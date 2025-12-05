dddf9396f00532ab9dd1fbe635c1522fdc34702c
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-05 09:47:46 +0530

    final de boss

--- for-loop.py
+++ for-loop.py
@@ -1,32 +1,40 @@
-# # for loop to add the first n numbers 
+# # # for loop to add the first n numbers 
 
-# # num : int  = int (input ('enter the number'))
-# # sum = 0
-# # for i in range (1,num +1):
-# #     sum += i
+# # # num : int  = int (input ('enter the number'))
+# # # sum = 0
+# # # for i in range (1,num +1):
+# # #     sum += i
 
-# # print (sum)
+# # # print (sum)
 
-# # odd numbers
-# num = int(input('enter the number bete: '))
-# for i in range (1, num +1):
-#     if i % 2 != 0:
-#         print (i)
-
-
-
-# # even numbers
-# num = int(input('enter the number bete: '))
-# for i in range (1, num +1):
-#     if i % 2 == 0:
-#         print (i)
+# # # odd numbers
+# # num = int(input('enter the number bete: '))
+# # for i in range (1, num +1):
+# #     if i % 2 != 0:
+# #         print (i)
 
 
 
-# num = int (input ('Enter the number: '))
-# for i in range (num +1, 0, -1):
-#     print (i)
+# # # even numbers
+# # num = int(input('enter the number bete: '))
+# # for i in range (1, num +1):
+# #     if i % 2 == 0:
+# #         print (i)
 
-country = 'India'
-for i in country:
-    print (i , end= ' ')
+
+
+# # num = int (input ('Enter the number: '))
+# # for i in range (num +1, 0, -1):
+# #     print (i)
+
+# country = 'India'
+# for i in country:
+#     print (i , end= ' ')
+
+day = 21
+month = 2
+year = 2025
+
+print ('the date is', end =' ' )
+
+print (day, month, year, sep = '/')
