5feab4e9b8d251409022473692f372b37413dd08
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 20:57:29 +0530

    ts we go

--- loops/02.py
+++ loops/02.py
@@ -1,23 +1,30 @@
-# for i in range (1, 4):
-#     for y in range (1,10):
-#         print (y,end = '')
+# # for i in range (1, 4):
+# #     for y in range (1,10):
+# #         print (y,end = '')
+# #     print ()
+
+
+# # square printing
+
+# # any patterns -> outer loop for the lines 
+
+# # inner loop for the number of elements
+
+# num : int = int (input ('Enter the number: '))
+# for i in range (1, num):
+#     for j in range (1, num):
+#         print ('*', end = ' ')
 #     print ()
 
 
-# rectangle 
 
-# any patterns -> outer loop for the lines 
+# # for i in range (1, 6):
+# #     print ("* " * i)
 
-# inner loop for the number of elements
 
-num : int = int (input ('Enter the number: '))
-for i in range (1, num):
-    for j in range (1, num):
-        print ('*', end = ' ')
+# rectangle printing 
+
+for i in range (5):
+    for j in range (4):
+        print ("*", end = ' ')
     print ()
-
-
-
-# for i in range (1, 6):
-#     print ("* " * i)
-
