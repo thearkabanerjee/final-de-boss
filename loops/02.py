4157bc65e450012021d67fa95b813c9aad362bcc
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 20:57:58 +0530

    ts we go

--- loops/02.py
+++ loops/02.py
@@ -24,7 +24,9 @@
 
 # rectangle printing 
 
-for i in range (5):
-    for j in range (4):
-        print ("*", end = ' ')
-    print ()
+# for i in range (5):
+#     for j in range (4):
+#         print ("*", end = ' ')
+#     print ()
+
+
