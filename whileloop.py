8ae3c43c2aa2b728f4a7dcc0b683ed2b7455e23c
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 16:10:11 +0530

    py we go

--- whileloop.py
+++ whileloop.py
@@ -1,7 +1,16 @@
-# while loops for printing namws 
+# # while loops for printing namws 
 
-name : str = str (input('Enter your name: '))
-while (name == ''):
-    print ('you didnt enter your name')
-    name : str = str (input ('Enter your name: '))
-print (f'Hello, {name}')
+# name : str = str (input('Enter your name: '))
+# while (name == ''):
+#     print ('you didnt enter your name')
+#     name : str = str (input ('Enter your name: '))
+# print (f'Hello, {name}')
+
+
+# while loops for printing string 5 times 
+
+name : str = str(input ('Enter your name: '))
+num : int = int (input ('enter times'))
+while (num <= 5):
+    print (name)
+    num +=1
