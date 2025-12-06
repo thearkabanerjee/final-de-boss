d9c40e0fe84b23bf28342b2300e90d1627b1af81
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 12:40:40 +0530

    py  we go

---
+++ number-of-digits.py
@@ -0,0 +1,23 @@
+# # find the number of digits in. the given number
+
+# number : int = int (input ('ENter the number yuou want '))
+# numbersinstring : str = str (number)
+
+
+# # digits : str = '0123456789'.split()
+
+# # for char in numbersinstring:
+# #     if char in digits:
+# #         print (digits)
+
+
+# # print (number, numbersinstring, digits )
+
+
+# print (len (numbersinstring))
+
+
+
+# find the number of digits in a given number
+
+number: int = int (input())
