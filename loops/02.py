9b85f30c7218e67c2fa3f3b9dc406d5ff142d4d6
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-06 20:52:32 +0530

    ts we go

--- loops/02.py
+++ loops/02.py
@@ -6,3 +6,18 @@
 
 # rectangle 
 
+# any patterns -> outer loop for the lines 
+
+# inner loop for the number of elements
+
+num : int = int (input ('Enter the number: '))
+for i in range (1, num):
+    for j in range (1, num):
+        print ('*', end = ' ')
+    print ()
+
+
+
+# for i in range (1, 6):
+#     print ("* " * i)
+
